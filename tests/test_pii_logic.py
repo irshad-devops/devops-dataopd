2a961eeb9c66692e983887155f7fdd65b8683a73
irshad-devops <imarwatshad5@gmail.com>
2026-01-05 22:17:58 +0500

    Demo: Enhancing PII masking validation logic22

--- tests/test_pii_logic.py
+++ tests/test_pii_logic.py
@@ -13,12 +13,3 @@ def test_spark_session():
     assert df.count() == 2
     spark.stop()
 
-def test_pii_masking_integrity():
-    # Simulate a check for masked data
-    sample_email = "user@example.com"
-    masked_email = "********@example.com" 
-    
-    # Assert that the masking actually happened
-    assert "@" in masked_email
-    assert "user" not in masked_email
-    print("PII Masking Validation Passed!")
